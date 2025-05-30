67ac83ac33d4f4087265002163f8aa5be942c644
dahxxn <dahyeonhan4529@gmail.com>
2025-05-30 11:02:19 +0900

    feat: repository 객체 추가

--- src/main/java/com/walkbook/demo/service/BookServiceImpl.java
+++ src/main/java/com/walkbook/demo/service/BookServiceImpl.java
@@ -1,9 +1,12 @@
 package com.walkbook.demo.service;
 
+import com.walkbook.demo.repository.BookRepository;
+import com.walkbook.demo.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class BookServiceImpl implements BookService{
+    private final BookRepository bookRepository;
 }
