c21e089623aca6ee97c2c472056f772e36597c4b
dahxxn <dahyeonhan4529@gmail.com>
2025-05-30 10:59:50 +0900

    feat: Book Controller 추가

---
+++ src/main/java/com/walkbook/demo/controller/BookController.java
@@ -0,0 +1,11 @@
+package com.walkbook.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/categories")
+public class BookController {
+}
